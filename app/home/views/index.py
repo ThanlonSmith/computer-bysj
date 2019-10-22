996e74c3c5cc72453106fb29a1477e1931825b88
thanlon <thanlon@163.com>
2019-10-23 01:08:38 +0800

    Update the "layout.html" and add the "index.html" page

--- app/home/views/index.py
+++ app/home/views/index.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/10/21 上午2:03
+# @Time    : 2019/10/23 上午12:59
 # @Author  : Thanlon
 # @Wechat：18512152005
 # @Email   : thanlon@sina.com
-# @File    : user.py
+# @File    : index.py
 # @Software: PyCharm
 from .. import home_bp
 from flask import render_template
 
 
-# @home_bp.route('/login')
-# def login():
-#     return render_template('home/layout.html')
+@home_bp.route('/')
+def index():
+    return render_template('/home/index.html')
