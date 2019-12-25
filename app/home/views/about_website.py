ca6622b678df5bbb1675c6db6d3fe9ece8d16b2b
thanlon <thanlon@163.com>
2019-12-25 16:31:41 +0800

    Added "about this website" page.

---
+++ app/home/views/about_website.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/25 下午3:46
+# @Author  : Thanlon
+# @Wechat：18512152005
+# @Email   : thanlon@sina.com
+# @File    : about_website.py
+# @Software: PyCharm
+from .. import home_bp
+from flask import render_template
+@home_bp.route('/v1/about-website')
+def about_website():
+    return render_template('/home/about-website.html')
